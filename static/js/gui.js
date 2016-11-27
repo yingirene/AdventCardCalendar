adab93d789651af79c7fdf111382c5902a223b66
Irene Ying <iying@ucsd.edu>
2016-11-27 14:45:37 -0800

    Detailing Calendar

--- static/js/gui.js
+++ static/js/gui.js
@@ -1,3 +1,20 @@
+window.addEventListener("load", function() {
+    var today = new Date();
+    var xmasDay = new Date();
+    xmasDay.setMonth(11);
+    xmasDay.setDate(25);
+    var daysLeft = new Date(xmasDay.getTime() - today.getTime())/1000/60/60/24;
+    if(today.getMonth == 11) {
+        $("#closedSign").show();
+        $("#openSign").hide();
+        $("#closedSign").children()[1].prepend(daysLeft);
+    } else {
+        $("#closedSign").hide();
+        $("#openSign").show();
+        $("#openSign").children()[0].prepend(daysLeft);
+    }
+}, false);
+
 $("#calBtn").click(function(e) {
     $("#calendar").toggleClass("slide-up");
 });
@@ -8,4 +25,8 @@ $("#subBtn").click(function(e) {
 
 $("#propBtn").click(function(e) {
     $("#properties").toggleClass("hidden");
+});
+
+$("#calendar li").one("click", function(e) {
+    $(this).addClass("opened");
 });
